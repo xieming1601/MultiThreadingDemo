4685e6a31f44153e2b5476c5a99398fe6d956ddb
xsm <xieming1601@163.com>
2016-09-11 16:46:59 +0800

    init

---
+++ MultiThreadingDemo/MultiThreadingDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultiThreadingDemo
+//
+//  Created by xsm on 16/9/11.
+//  Copyright © 2016年 xsm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
